38a8a5ebdaf39a2baa0e6ca5046e699300e0d1f2
nasoviva <nasoviva@gmail.com>
2024-07-02 21:59:54 -0300

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by hverda on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
