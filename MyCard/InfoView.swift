0f73b80bcc67083bb8e19c510ed4e5ef63aaac02
nasoviva <nasoviva@gmail.com>
2024-07-03 21:56:54 -0300

    ready

---
+++ MyCard/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  MyCard
+//
+//  Created by hverda on 03/07/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
